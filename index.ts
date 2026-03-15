05a63f7a57a40782066117ef4cce650a2b835f60
Dashiev Roman <roman.daschiev@gmail.com>
2026-03-15 16:11:34 +0700

    Написал интерфейс и функцию

---
+++ index.ts
@@ -0,0 +1,11 @@
+interface User {
+    id: number;
+    name: string;
+    email?: string;
+    isActive: boolean;
+}
+
+function createUser(id: number, name: string, email?: string, isActive: boolean = true): User {
+    return { id, name, email, isActive};
+}
+
