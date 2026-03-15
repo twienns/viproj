69877ed8992e437e1630d62ebd2b3d9aa3e2dad8
Dashiev Roman <roman.daschiev@gmail.com>
2026-03-15 16:41:29 +0700

    Написал интерфейс Book и функцию createBook

--- index.ts
+++ index.ts
@@ -8,4 +8,22 @@ interface User {
 function createUser(id: number, name: string, email?: string, isActive: boolean = true): User {
     return { id, name, email, isActive};
 }
+type Genre = 'fiction' | 'non-fiction';
 
+interface Book {
+    title: string;
+    author: string;
+    year?: number;
+    genre: Genre;
+}
+
+function creatBook(book: Book): Book {
+    return book;
+}
+
+const book1: Book = {
+    title: 'Colorless Tsukuru Tazaki and His Years of Wandering',
+    author: 'Haruki Murakami',
+    genre: 'non-fiction',
+};
+console.log(creatBook(book1));
